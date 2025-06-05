63857544d73200ebbe18aa21143ae92d6c181116
Poralia <poraliaglobal@gmail.com>
2025-06-05 21:43:41 +0700

    Added allowed host for rtbs.asciiutf.com

--- src/application/resturanttablebs/settings.py
+++ src/application/resturanttablebs/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-@gcoyeg+mkq^d%#j$^g%o0=2y0)su)w7v@t1mc_=q*2oi8!*=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rtbs.asciiutf.com']
 
 
 # Application definition
