76fbfc0a9949aeb02863151d9bcc7df0bcf1ad72
Poralia <poraliaglobal@gmail.com>
2025-06-07 15:54:05 +0700

    Manu page content to webpage settings

--- src/application/configsapp/forms.py
+++ src/application/configsapp/forms.py
@@ -6,3 +6,4 @@ class ConstanteForm(forms.Form):
     background_image = forms.ImageField(required=False)
     logo_image = forms.ImageField(required=False)
     background_color_booking_form = forms.CharField(required=False)
+    manu_page_content = forms.FileField(required=False)
