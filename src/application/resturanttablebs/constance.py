b50910df49f52736200db5bb6e66cc1880df8685
Poralia <poraliaglobal@gmail.com>
2025-06-07 09:31:35 +0700

    Change navbar

--- src/application/resturanttablebs/constance.py
+++ src/application/resturanttablebs/constance.py
@@ -5,6 +5,8 @@ CONSTANCE_ADDITIONAL_FIELDS = {
 }
 
 CONSTANCE_CONFIG = {
+    'LOGO_IMAGE': ('', 'A logo for this website', 'image_field'),
+    'TAGLINE': ('Resturant Table Booking System', 'Change website tagline', str),
     'BACKGROUND_IMAGE': ('', 'Change background image webpage', 'image_field'),
     'BACKGROUND_COLOR_BOOKING_FORM': ('', 'Booking form background color', str),
 }
